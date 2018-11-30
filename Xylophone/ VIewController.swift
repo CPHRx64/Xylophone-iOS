67f8bde1d7c1da914e4f73798fd207c3219251e1
Cao Do <gusdocat@student.gu.se>
2018-11-30 20:11:04 +0100

    app finished

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
+    var audioPlayer : AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+ 
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        playSound(sender : sender)
     }
     
-  
+    func playSound (sender : UIButton) {
+        let note = tag2Note(tag: sender.tag)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: note)
+        } catch {
+            print(description)
+        }
+        audioPlayer.play()
+    }
+    
+    func tag2Note (tag : Int) -> URL {
+        var result : URL
+        switch tag {
+            case 1:
+                result = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+            case 2:
+                result = Bundle.main.url(forResource: "note2", withExtension: "wav")!
+            case 3:
+                result = Bundle.main.url(forResource: "note3", withExtension: "wav")!
+            case 4:
+                result = Bundle.main.url(forResource: "note4", withExtension: "wav")!
+            case 5:
+                result = Bundle.main.url(forResource: "note5", withExtension: "wav")!
+            case 6:
+                result = Bundle.main.url(forResource: "note6", withExtension: "wav")!
+            default:
+                result = Bundle.main.url(forResource: "note7", withExtension: "wav")!
+        }
+        return result
+    }
 
 }
 
